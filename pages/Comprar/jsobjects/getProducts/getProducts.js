52a93bb5cfcc74ad8df1a7a7620233ad9bb3b657
DANIEL TIEPOLO KOCHINSKI <kochinski@alunos.utfpr.edu.br>
2024-11-26 22:48:22 +0000

    .

--- pages/Comprar/jsobjects/getProducts/getProducts.js
+++ pages/Comprar/jsobjects/getProducts/getProducts.js
@@ -4,16 +4,17 @@ export default {
             const result = await getProductsQuery.run(); // Fetch product data
 
             const products = result.map((p) => ({
-                Product_ID: p.product_id,
-                Name: p.name,
-                SKU: p.sku,
-                Category: p.category || "N/A",
-                PriceRaw: parseFloat(p.price || 0), // Raw price as float
-                Price: parseFloat(p.price || 0).toLocaleString("pt-PT", {
+
+                Valor: parseFloat(p.price || 0).toLocaleString("pt-PT", {
                     style: "currency",
                     currency: "EUR",
                 }), // Formatted price for display
+                Nome: p.name,
+                SKU: p.sku,
+                Categoria: p.category || "N/A",
                 Stock: p.total_stock,
+							 	PriceRaw: parseFloat(p.price || 0), // Raw price as float
+								ID: p.product_id,
             }));
 
             Table_products.setData(products); // Set product table data
@@ -27,5 +28,6 @@ export default {
 // ------------------------------------------------------------
 // getProducts.js - Fetches and formats product data.
 // ------------------------------------------------------------
+
 
 
